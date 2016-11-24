f899cba50683e67f530341529de260a6c7949069
TheTimelord <unconfigured@null.spigotmc.org>
2016-11-23 21:41:03 -0500

    Minimum Viable Product - Simple Platformer
    Simple platformer functionality added: collision, jumping, etc.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.1f;
+	public LayerMask whatIsGround;
+	public Vector2 jumpHeight;
+	private Rigidbody2D rb2d;
+	public int speed;
+
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate () {
+
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal * speed, rb2d.velocity.y);
+		rb2d.velocity = movement;
+	}
+
+	void Update()
+	{
+		if (grounded && Input.GetKeyDown(KeyCode.W))
+		{
+			GetComponent<Rigidbody2D>().AddForce(jumpHeight, ForceMode2D.Impulse);
+		}
+	}
+}
