f66da63f0d2be33fdba422a1924e507002bb21fb
TheTimelord <unconfigured@null.spigotmc.org>
2016-11-24 21:34:48 -0500

    Modified Physics Engine
    Changed from Input.GetAxis system for physics to a linear acceleration
    system, along with adding in deceleration and removing friction.  Since
    no friction means no need for physics materials, those were removed.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,7 +9,11 @@ public class PlayerController : MonoBehaviour {
 	public LayerMask whatIsGround;
 	public Vector2 jumpHeight;
 	private Rigidbody2D rb2d;
-	public int speed;
+
+	public float maxSpeed = 5f;
+	public float deceleration = .8f;
+	public float acceleration = .5f;
+	public float airAccelMod = .8f;
 
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
@@ -19,15 +23,49 @@ public class PlayerController : MonoBehaviour {
 
 		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
 
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		Vector2 movement = new Vector2 (moveHorizontal * speed, rb2d.velocity.y);
-		rb2d.velocity = movement;
+		//NOTE: KeyCode needs to be replaced with input manager in later version.
+
+		float velocity = rb2d.velocity.x;
+
+		if (Input.GetKey(KeyCode.D)) {
+			if ((!grounded) && (velocity > 0)) {
+				velocity = velocity + (acceleration * airAccelMod);
+			}
+			else {
+				velocity = velocity + acceleration;
+			}
+		}
+
+		if (Input.GetKey(KeyCode.A)) {
+			if ((!grounded) && (velocity < 0)) {
+				velocity = velocity - (acceleration * airAccelMod);
+			}
+			else {
+				velocity = velocity - acceleration;
+			}
+		}
+
+		if (Mathf.Abs (velocity) > maxSpeed) {
+			//Ternary operator: Google it.
+			velocity = velocity > 0 ? maxSpeed : -maxSpeed;
+		}
+
+		rb2d.velocity = new Vector2(velocity, rb2d.velocity.y);
+
+		if (!Input.GetKey(KeyCode.D) && (!Input.GetKey(KeyCode.A))) {
+			float newVelocity = rb2d.velocity.x * deceleration;
+			rb2d.velocity = new Vector2(newVelocity, rb2d.velocity.y);
+		}
+
+		if (Mathf.Abs (rb2d.velocity.x) < .001) {
+			rb2d.velocity = new Vector2 (0, rb2d.velocity.y);
+		}
+			
 	}
 
 	void Update()
 	{
-        if (grounded && (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)))
-        {
+		if (grounded && (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space))) {
             GetComponent<Rigidbody2D>().AddForce(jumpHeight, ForceMode2D.Impulse);
         }
 	}
