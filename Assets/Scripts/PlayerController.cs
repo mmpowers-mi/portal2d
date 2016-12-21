cc9d9f317c3c0ddf77e8344ad21f9ea4ec3654f1
TheTimelord <unconfigured@null.spigotmc.org>
2016-12-20 19:24:51 -0500

    Added Portals
    Portals added, physics tweaks.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,12 +3,26 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	public GameObject portalOrangeObj;
+	public GameObject portalBlueObj;
+
 	bool grounded = false;
+	bool touchingPortalBlue = false;
+	bool touchingPortalOrange = false;
 	public Transform groundCheck;
+	public Transform portalCheck;
 	float groundRadius = 0.1f;
+	float portalRadius = 0.1f;
 	public LayerMask whatIsGround;
+	public LayerMask portalBlue;
+	public LayerMask portalOrange;
 	public Vector2 jumpHeight;
 	private Rigidbody2D rb2d;
+
+	public int portalOrangeDir = 1;
+	public int portalBlueDir = 1;
+
+	public float portalExitOffset = .2f;
 
 	public float maxSpeed = 5f;
 	public float deceleration = .8f;
@@ -17,11 +31,61 @@ public class PlayerController : MonoBehaviour {
 
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
+		rb2d.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+		Physics2D.IgnoreLayerCollision(8, 9, true);
+		//portalOrangeObj = GameObject.FindGameObjectWithTag("PortalOrange");
+		//portalBlueObj = GameObject.FindGameObjectWithTag("PortalBlue");
+
 	}
 
 	void FixedUpdate () {
 
 		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+		touchingPortalBlue = Physics2D.OverlapCircle (portalCheck.position, portalRadius, portalBlue);
+		touchingPortalOrange = Physics2D.OverlapCircle (portalCheck.position, portalRadius, portalOrange);
+
+		if (touchingPortalBlue) {
+			this.transform.position = portalOrangeObj.transform.position;
+			Vector2 tempPosition = this.transform.position;
+			if (portalOrangeDir == 0) {
+				tempPosition.y = tempPosition.y + portalExitOffset;
+			}
+
+			if (portalOrangeDir == 1) {
+				tempPosition.x = tempPosition.x + portalExitOffset;
+			}
+
+			if (portalOrangeDir == 2) {
+				tempPosition.y = tempPosition.y - portalExitOffset;
+			}
+
+			if (portalOrangeDir == 3) {
+				tempPosition.x = tempPosition.x - portalExitOffset;
+			}
+			this.transform.position = tempPosition;
+		}
+		if (touchingPortalOrange) {
+			this.transform.position = portalBlueObj.transform.position;
+			Vector2 tempPosition = this.transform.position;
+			if (portalBlueDir == 0) {
+				tempPosition.y = tempPosition.y + portalExitOffset;
+			}
+
+			if (portalBlueDir == 1) {
+				tempPosition.x = tempPosition.x + portalExitOffset;
+			}
+
+			if (portalBlueDir == 2) {
+				tempPosition.y = tempPosition.y - portalExitOffset;
+			}
+
+			if (portalBlueDir == 3) {
+				tempPosition.x = tempPosition.x - portalExitOffset;
+			}
+			this.transform.position = tempPosition;
+		}
+
+
 
 		//NOTE: KeyCode needs to be replaced with input manager in later version.
 
@@ -59,6 +123,16 @@ public class PlayerController : MonoBehaviour {
 
 		if (Mathf.Abs (rb2d.velocity.x) < .001) {
 			rb2d.velocity = new Vector2 (0, rb2d.velocity.y);
+		}
+
+		Vector2 tempVelocity = rb2d.velocity;
+		if (portalBlueDir == portalOrangeDir) {
+			if (portalBlueDir == 0 || portalBlueDir == 2) {
+				tempVelocity.y = tempVelocity.y * -1;
+			}
+			if (portalBlueDir == 1 || portalBlueDir == 3) {
+				tempVelocity.x = tempVelocity.x * -1;
+			}
 		}
 			
 	}
